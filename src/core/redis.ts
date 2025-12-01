bf52a17c71b83a58df269ba7971f4f53435699ec
瑜笙 <2624367622@qq.com>
2025-12-01 21:13:27 +0800

    fix: 临时解决Bot不显示发送消息次数的问题

--- src/core/redis.ts
+++ src/core/redis.ts
@@ -1,5 +1,7 @@
 import { hooks, redis } from 'node-karin'
 
+const RECV_MSG = 'nekostatus:receive'
+const SEND_MSG = 'nekostatus:send'
 let isinit = false
 /**
  * 获取指定Bot接收消息次数
@@ -7,25 +9,33 @@ let isinit = false
  * @returns
  */
 export const getReceiveCount = async (Id: string) => {
-  const d1 = await redis.get(`nekostatus:receive:${Id}`)
+  const d1 = await redis.get(`${RECV_MSG}:${Id}`)
   if (!d1) return 0
   return JSON.parse(d1).count as number
 }
-// TODO: 发送消息次数统计无法实现
 /**
  * 获取指定Bot发送消息次数
  * @param Id BotId
  * @returns
  */
 export const getSendCount = async (Id: string) => {
-  return 0
+  const d1 = await redis.get(`${SEND_MSG}:${Id}`)
+  if (!d1) return 0
+  return JSON.parse(d1).count as number
 }
 const init = () => {
   if (isinit) return
   isinit = true
   hooks.message(async (event, next) => {
     try {
-      const d1 = await redis.get(`nekostatus:receive:${event.bot.selfId}`)
+      let key
+      // TODO: 临时监听消息判断发送者是否是本身来判断发送消息次数
+      if (event.selfId === event.sender.userId) {
+        key = `${SEND_MSG}:${event.bot.selfId}`
+      } else {
+        key = `${RECV_MSG}:${event.bot.selfId}`
+      }
+      const d1 = await redis.get(key)
       const now = new Date()
       const time = now.getFullYear() * 100 + now.getMonth() + 1
       let d2: { count: number, time: number } = d1 ? JSON.parse(d1) : { count: 0, time }
@@ -33,7 +43,7 @@ const init = () => {
         d2 = { count: 0, time }
       }
       d2.count += 1
-      redis.set(`nekostatus:receive:${event.bot.selfId}`, JSON.stringify(d2))
+      redis.set(key, JSON.stringify(d2))
     } finally {
       next()
     }
--- src/modules/process.ts
+++ src/modules/process.ts
@@ -1,59 +1,68 @@
-import { format } from '@/utils'
-import si from 'systeminformation'
-/**
- * 获取进程信息
- * @param opt
- * @returns
- */
-export async function getProcessInfo (opt: { sortBy: 'cpu' | 'mem' } = { sortBy: 'mem' }) {
-  const p = await si.processes()
-  const f = p.list.filter(proc => proc.name !== 'System Idle Process')
-  interface ob {
-    name: string,
-    pid: string,
-    cpu: number,
-    mem: number,
-    count: number
-  }
-  const pm = new Map<number, ob>()
-  f.forEach(p => {
-    const Pid = p.parentPid || p.pid
-    if (!pm.has(Pid)) {
-      pm.set(Pid, {
-        name: p.name,
-        pid: p.pid.toString(),
-        cpu: p.cpu,
-        mem: p.mem,
-        count: 1
-      })
-    } else {
-      const pd = pm.get(Pid)
-      if (pd) {
-        pd.cpu += p.cpu
-        pd.mem += p.mem
-        pd.count++
-        pd.pid += `,${p.pid}`
-      }
-    }
-  })
-  const pl = Array.from(pm.values())
-  const sp = pl.sort((a, b) => {
-    const sf = opt.sortBy === 'mem' ? 'mem' : 'cpu'
-    return b[sf] - a[sf]
-  })
-  const l = sp.slice(0, 10).map(p => ({
-    name: p.name,
-    count: p.count - 1,
-    cpu: p.cpu,
-    mem: format(p.mem),
-    pid: p.pid
-  }))
-  return {
-    all: p.all,
-    running: p.running,
-    blocked: p.blocked,
-    sleeping: p.sleeping,
-    unknown: p.unknown,
-    list: l
-  }
-}
+// import { format } from '@/utils'
+// import { isWin } from 'node-karin'
+// import si from 'systeminformation'
+// const prList = ['node', 'chrome', 'chromium', 'redis-server', `${process.title}`]
+// /**
+//  * 获取进程信息
+//  * @param opt
+//  * @returns
+//  */
+// export async function getProcessInfo (opt: { sortBy: 'cpu' | 'mem' } = { sortBy: 'mem' }) {
+//   const p = await si.processes()
+//   const f = p.list.filter(proc => proc.name !== 'System Idle Process')
+//   const l = f.filter(i => prList.includes(isWin ? i.name.replace(/\.exe$/i, '').toLocaleLowerCase() : i.name.toLocaleLowerCase())
+//   )
+// }
+
+// export async function _getProcessInfo (opt: { sortBy: 'cpu' | 'mem' } = { sortBy: 'mem' }) {
+//   const p = await si.processes()
+//   const f = p.list.filter(proc => proc.name !== 'System Idle Process')
+//   interface ob {
+//     name: string,
+//     pid: string,
+//     cpu: number,
+//     mem: number,
+//     count: number
+//   }
+//   const pm = new Map<number, ob>()
+//   f.forEach(p => {
+//     const Pid = p.parentPid || p.pid
+//     if (!pm.has(Pid)) {
+//       pm.set(Pid, {
+//         name: p.name,
+//         pid: p.pid.toString(),
+//         cpu: p.cpu,
+//         mem: p.mem,
+//         count: 1
+//       })
+//     } else {
+//       const pd = pm.get(Pid)
+//       if (pd) {
+//         pd.cpu += p.cpu
+//         pd.mem += p.mem
+//         pd.count++
+//         pd.pid += `,${p.pid}`
+//       }
+//     }
+//   })
+//   const pl = Array.from(pm.values())
+//   const sp = pl.sort((a, b) => {
+//     const sf = opt.sortBy === 'mem' ? 'mem' : 'cpu'
+//     return b[sf] - a[sf]
+//   })
+//   const l = sp.slice(0, 10).map(p => ({
+//     name: p.name,
+//     count: p.count - 1,
+//     cpu: p.cpu,
+//     mem: format(p.mem),
+//     pid: p.pid
+//   }))
+//   return {
+//     all: p.all,
+//     running: p.running,
+//     blocked: p.blocked,
+//     sleeping: p.sleeping,
+//     unknown: p.unknown,
+//     list: l
+//   }
+// }
