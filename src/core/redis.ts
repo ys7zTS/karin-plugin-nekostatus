ac90284544f34ee245701a1999ec7f0d26ad7cc4
瑜笙 <2624367622@qq.com>
2025-12-02 11:41:30 +0800

    fix: 优化Redis 消息统计逻辑

--- src/core/redis.ts
+++ src/core/redis.ts
@@ -1,7 +1,9 @@
 import { hooks, redis } from 'node-karin'
 
+/** 接收消息key */
 const RECV_MSG = 'nekostatus:receive'
-const SEND_MSG = 'nekostatus:send'
+/** 发送消息key */
+// const SEND_MSG = 'nekostatus:send'
 let isinit = false
 /**
  * 获取指定Bot接收消息次数
@@ -9,46 +11,38 @@ let isinit = false
  * @returns
  */
 export const getReceiveCount = async (Id: string) => {
-  const d1 = await redis.get(`${RECV_MSG}:${Id}`)
-  if (!d1) return 0
-  return JSON.parse(d1).count as number
+  const key = `${RECV_MSG}:${getMonth()}:${Id}`
+  const d1 = await redis.get(key)
+  return +(d1 || 0)
 }
+// TODO: 无法统计Bot消息发送量
 /**
  * 获取指定Bot发送消息次数
  * @param Id BotId
  * @returns
  */
 export const getSendCount = async (Id: string) => {
-  const d1 = await redis.get(`${SEND_MSG}:${Id}`)
-  if (!d1) return 0
-  return JSON.parse(d1).count as number
+  return 0
 }
-const init = () => {
+/** 获取年月 */
+const getMonth = () => {
+  const now = new Date()
+  return now.getFullYear() * 100 + now.getMonth() + 1
+}
+/** 初始化 */
+export const init = async () => {
   if (isinit) return
   isinit = true
   hooks.message(async (event, next) => {
     try {
-      let key
-      // TODO: 临时监听消息判断发送者是否是本身来判断发送消息次数
-      if (event.selfId === event.sender.userId) {
-        key = `${SEND_MSG}:${event.bot.selfId}`
-      } else {
-        key = `${RECV_MSG}:${event.bot.selfId}`
-      }
-      const d1 = await redis.get(key)
-      const now = new Date()
-      const time = now.getFullYear() * 100 + now.getMonth() + 1
-      let d2: { count: number, time: number } = d1 ? JSON.parse(d1) : { count: 0, time }
-      if (d2.time !== time) {
-        d2 = { count: 0, time }
-      }
-      d2.count += 1
-      redis.set(key, JSON.stringify(d2))
+      const key = `${RECV_MSG}:${getMonth()}:${event.bot.selfId}`
+      redis.incr(key)
+      redis.expire(key, 60 * 60 * 24 * 35)
     } finally {
       next()
     }
   })
-  // TODO: 无法获取Bot本身ID用来统计消息量
+  // TODO: 该函数无法获取Bot本身ID用来统计消息量
   // hooks.sendMsg.message(async (contact, _, __, next) => {
   //   try {
   //     const d1 = await redis.get(`nekostatus:send:${contact.peer}`)
@@ -57,4 +51,3 @@ const init = () => {
   //   }
   // })
 }
-init()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { dir } from './utils/dir'
 import { logger } from 'node-karin'
-import '@/core'
+import { init } from '@/core'
+await init()
 logger.info(`${logger.violet(`[插件:${dir.version}]`)} ${logger.green(dir.name)} 初始化完成~`)
